b2851916b0d426cf7e0fba7511205bd19dd8ec1e
Nadya <hadycr@gmail.com>
2023-05-19 23:33:28 +0400

    13 работа

--- controllers/users.js
+++ controllers/users.js
@@ -38,11 +38,11 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Данные не найдены' });
+        return res.status(400).send({ message: 'Данные не корректны' });
       }
       // if (err.name === 'CastError') {
       //   return res.status(400).send({ message: 'Данные не корректны' });
@@ -53,7 +53,7 @@ module.exports.updateUser = (req, res) => {
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch((err) => {
       if (err.name === 'CastError') {
